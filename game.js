7480f5d03849172716cbd4de50eff7ea8aa49fe4
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2019-08-14 00:26:22 +0300

    Added images and classes

--- game.js
+++ game.js
@@ -1,32 +1,39 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.getElementById("canvas"); //Получение холста из DOM
+var ctx = canvas.getContext("2d"); //Получение контекста — через него можно работать с холстом
 
-Resize();
+Resize(); // При загрузке страницы задаётся размер холста
 
-window.addEventListener("resize", Resize);
-canvas.addEventListener("contextmenu", function (e) { e.preventDefault(); return false; });
+window.addEventListener("resize", Resize); //При изменении размеров окна будут менять размеры холста
 
-window.addEventListener("keydown", function (e) { KeyDown(e); });
+//Запрет на вызов контекстного меню
+//Он нужен для того, чтобы при долгом нажатии на хост с мобильных устройств, не вылезало меню и не мешало играть
+canvas.addEventListener("contextmenu", function (e) { e.preventDefault(); return false; }); 
 
-var objects = [];
+window.addEventListener("keydown", function (e) { KeyDown(e); }); //Получение нажатий с клавиатуры
 
-function Update()
+var objects = []; //Массив игровых объектов
+
+var player = null; //объект, которым управляет игрок
+
+function Start()
 {
+	timer = setInterval(Update, 1000 / 60); //Состояние игры будет обновляться 60 раз в секунду — при такой частоте обновление происходящее будет казаться очень плавным
 
+}
 
+function Stop()
+{
+	clearInterval(timer); //Остановка обновления
+}
+
+function Update() //Обновление игры
+{
 	Draw();
 }
 
-function Draw()
+function Draw() //Работа с графикой
 {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-
-}
-
-function Control()
-{
-
+	ctx.clearRect(0, 0, canvas.width, canvas.height); //Очиста холста от предыдущего кадра
 }
 
 function KeyDown(e)
@@ -44,6 +51,9 @@ function KeyDown(e)
 
 		case 40: //Вниз
 			break;
+
+		case 27: //Вниз
+			break;
 	}
 }
 
@@ -51,4 +61,45 @@ function Resize()
 {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
+}
+
+class Road
+{
+	constructor(image, y)
+	{
+		this.x = 0;
+		this.y = y;
+		this.loaded = false;
+
+		this.image = new Image();
+		
+		var obj = this;
+
+		this.image.addEventListener("load", function () { obj.loaded = true; });
+
+		this.image.src = image;
+	}
+
+	Update(road)
+	{
+		//logic
+	}
+}
+
+class Car
+{
+	constructor(image, x, y)
+	{
+		this.x = x;
+		this.y = y;
+		this.loaded = false;
+
+		this.image = new Image();
+
+		var obj = this;
+
+		this.image.addEventListener("load", function () { obj.loaded = true; });
+
+		this.image.src = image;
+	}
 }
