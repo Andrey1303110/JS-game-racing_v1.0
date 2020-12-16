2f85d57b09ba99c968f3d20bf26f35f373203540
Andrey1303110 <andrey1303110@gmail.com>
2020-12-16 18:34:56 +0200

    some fixes and done game with another car

--- game.js
+++ game.js
@@ -1,14 +1,12 @@
 
-class Road
-{
-	constructor(image, y)
-	{
+class Road {
+	constructor(image, y) {
 		this.x = 0;
 		this.y = y;
 		this.loaded = false;
 
 		this.image = new Image();
-		
+
 		var obj = this;
 
 		this.image.addEventListener("load", function () { obj.loaded = true; });
@@ -16,21 +14,18 @@ class Road
 		this.image.src = image;
 	}
 
-	Update(road) 
-	{
+	Update(road) {
 		this.y += speed; //The image will move down with every frame
 
-		if(this.y > window.innerHeight) //if the image left the screen, it will change it's position
+		if (this.y > window.innerHeight) //if the image left the screen, it will change it's position
 		{
-			this.y = road.y - canvas.width + speed; //New position depends on the second Road object
+			this.y = road.y - canvas.height + speed; //New position depends on the second Road object
 		}
 	}
 }
 
-class Car
-{
-	constructor(image, x, y, isPlayer)
-	{
+class Car {
+	constructor(image, x, y, isPlayer) {
 		this.x = x;
 		this.y = y;
 		this.loaded = false;
@@ -46,26 +41,22 @@ class Car
 		this.image.src = image;
 	}
 
-	Update()
-	{
-		if(!this.isPlayer)
-		{
+	Update() {
+		if (!this.isPlayer) {
 			this.y += speed;
 		}
 
-		if(this.y > canvas.height + 50)
-		{
+		if (this.y > canvas.height + 100) {
 			this.dead = true;
 		}
 	}
 
-	Collide(car)
-	{
+	Collide(car) {
 		var hit = false;
 
-		if(this.y < car.y + car.image.height * scale && this.y + this.image.height * scale > car.y) //If there is collision by y
+		if (this.y < car.y + (car.image.height - 25) * scale && this.y + this.image.height * scale > car.y) //If there is collision by y
 		{
-			if(this.x + this.image.width * scale > car.x && this.x < car.x + car.image.width * scale) //If there is collision by x
+			if (this.x + (this.image.width - 35) * scale > car.x && this.x < car.x + car.image.width * scale) //If there is collision by x
 			{
 				hit = true;
 			}
@@ -74,22 +65,19 @@ class Car
 		return hit;
 	}
 
-	Move(v, d) 
-	{
-		if(v == "x") //Moving on x
+	Move(v, d) {
+		if (v == "x") //Moving on x
 		{
-			d *= 2;
+			d *= 2.4;
 
 			this.x += d; //Changing position
 
 			//Rolling back the changes if the car left the screen
-			if(this.x + this.image.width * scale > canvas.width)
-			{
-				this.x -= d; 
+			if (this.x + this.image.width * scale > canvas.width) {
+				this.x -= d;
 			}
-	
-			if(this.x < 0)
-			{
+
+			if (this.x < 0) {
 				this.x = 0;
 			}
 		}
@@ -97,113 +85,101 @@ class Car
 		{
 			this.y += d;
 
-			if(this.y + this.image.height * scale > canvas.height)
-			{
+			if (this.y + this.image.height * scale > canvas.height) {
 				this.y -= d;
 			}
 
-			if(this.y < 0)
-			{
+			if (this.y < 0) {
 				this.y = 0;
 			}
 		}
-		
+
 	}
 }
 
 
-const UPDATE_TIME = 1000 / 60;
+const UPDATE_TIME = 1000 / 35;
 
 var timer = null;
 
 var canvas = document.getElementById("canvas"); //Getting the canvas from DOM
 var ctx = canvas.getContext("2d"); //Getting the context to work with the canvas
 
-var scale = 0.1; //Cars scale
+var scale = 0.25; //Cars scale
 
 Resize(); //Changing the canvas size on startup
 
 window.addEventListener("resize", Resize); //Change the canvas size with the window size
 
 //Forbidding openning the context menu to make the game play better on mobile devices
-canvas.addEventListener("contextmenu", function (e) { e.preventDefault(); return false; }); 
+canvas.addEventListener("contextmenu", function (e) { e.preventDefault(); return false; });
 
 window.addEventListener("keydown", function (e) { KeyDown(e); }); //Listenning for keyboard events
 
 var objects = []; //Game objects
 
-var roads = 
-[
-	new Road("images/road.jpg", 0),
-	new Road("images/road.jpg", canvas.width)
-]; //Backgrounds
+var roads =
+	[
+		new Road("images/road.jpg", 0),
+		new Road("images/road.jpg", canvas.height)
+	]; //Backgrounds
 
-var player = new Car("images/car.png", canvas.width / 2, canvas.height / 2, true); //Player's object
+var player = new Car("images/car1.png", canvas.width / 2, canvas.height * .76, true); //Player's object
 
 
-var speed = 5;
+var speed = 12;
 
 Start();
 
 
-function Start()
-{
-	if(!player.dead)
-	{
+function Start() {
+	if (!player.dead) {
 		timer = setInterval(Update, UPDATE_TIME); //Updating the game 60 times a second
 	}
-	
+
 }
 
-function Stop()
-{
+function Stop() {
 	clearInterval(timer); //Game stop
 	timer = null;
 }
 
-function Update() 
-{
+function Update() {
 	roads[0].Update(roads[1]);
 	roads[1].Update(roads[0]);
 
-	if(RandomInteger(0, 10000) > 9700) //Generating new car
+	if (RandomInteger(0, 10000) > 9780) //Generating new car
 	{
-		objects.push(new Car("images/car_red.png", RandomInteger(30, canvas.width - 50), RandomInteger(250, 400) * -1, false));
+		objects.push(new Car("images/car2.png", RandomInteger(40, canvas.width - 80), RandomInteger(200, 350) * -1, false));
 	}
 
 	player.Update();
 
-	if(player.dead)
-	{
+	if (player.dead) {
 		alert("Crash!");
 		Stop();
 	}
 
-	var isDead = false; 
+	var isDead = false;
 
-	for(var i = 0; i < objects.length; i++)
-	{
+	for (var i = 0; i < objects.length; i++) {
 		objects[i].Update();
 
-		if(objects[i].dead)
-		{
+		if (objects[i].dead) {
 			isDead = true;
 		}
 	}
 
-	if(isDead)
-	{
+	if (isDead) {
 		objects.shift();
 	}
 
 	var hit = false;
 
-	for(var i = 0; i < objects.length; i++)
-	{
+	for (var i = 0; i < objects.length; i++) {
 		hit = player.Collide(objects[i]);
 
-		if(hit)
-		{
+		if (hit) {
 			alert("Crash!");
 			Stop();
 			player.dead = true;
@@ -218,50 +194,45 @@ function Draw() //Working with graphics
 {
 	ctx.clearRect(0, 0, canvas.width, canvas.height); //Clearing the canvas
 
-	for(var i = 0; i < roads.length; i++)
-	{
+	for (var i = 0; i < roads.length; i++) {
 		ctx.drawImage
-		(
-			roads[i].image, //Image
-			0, //First X on image
-			0, //First Y on image
-			roads[i].image.width, //End X on image
-			roads[i].image.height, //End Y on image
-			roads[i].x, //X on canvas
-			roads[i].y, //Y on canvas
-			canvas.width, //Width on canvas
-			canvas.width //Height on canvas
-		);
+			(
+				roads[i].image, //Image
+				0, //First X on image
+				0, //First Y on image
+				roads[i].image.width, //End X on image
+				roads[i].image.height, //End Y on image
+				roads[i].x, //X on canvas
+				roads[i].y, //Y on canvas
+				canvas.width, //Width on canvas
+				canvas.height //Height on canvas
+			);
 	}
 
 	DrawCar(player);
 
-	for(var i = 0; i < objects.length; i++)
-	{
+	for (var i = 0; i < objects.length; i++) {
 		DrawCar(objects[i]);
 	}
 }
 
-function DrawCar(car)
-{
+function DrawCar(car) {
 	ctx.drawImage
-	(
-		car.image, 
-		0, 
-		0, 
-		car.image.width, 
-		car.image.height, 
-		car.x, 
-		car.y, 
-		car.image.width * scale, 
-		car.image.height * scale 
-	);
+		(
+			car.image,
+			0,
+			0,
+			car.image.width,
+			car.image.height,
+			car.x,
+			car.y,
+			car.image.width * scale,
+			car.image.height * scale
+		);
 }
 
-function KeyDown(e)
-{
-	switch(e.keyCode)
-	{
+function KeyDown(e) {
+	switch (e.keyCode) {
 		case 37: //Left
 			player.Move("x", -speed);
 			break;
@@ -279,27 +250,23 @@ function KeyDown(e)
 			break;
 
 		case 27: //Esc
-			if(timer == null)
-			{
+			if (timer == null) {
 				Start();
 			}
-			else
-			{
+			else {
 				Stop();
 			}
 			break;
 	}
 }
 
-function Resize()
-{
+function Resize() {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 }
 
-function RandomInteger(min, max) 
-{
-	let rand = min - 0.5 + Math.random() * (max - min + 1);
+function RandomInteger(min, max) {
+	let rand = min - 1 + Math.random() * (max - min + 1);
 	return Math.round(rand);
 }
 
